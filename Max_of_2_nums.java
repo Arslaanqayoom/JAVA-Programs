d3c7082d28cc6f197a14a33af9c68122bb36a311
Mohammad Arslaan <95520101+Arslaanqayoom@users.noreply.github.com>
2021-12-11 18:56:06 +0530

    Create Max_of_2_nums.java

---
+++ Max_of_2_nums.java
@@ -0,0 +1,22 @@
+import java.lang.Math;
+public class numbers {
+   public static void main(String[] args) 
+   {
+      //integer values
+      int a = 12;
+      int b = 10;
+      //float values
+      float c = 110.00f;
+      float d = 109.99f;
+      // double values
+      double e = 40.01d;
+      double f = 32.73d;
+      // long values
+      long g = 323456l;
+      long h = 55678l;
+      System.out.println("Max of " + a +" and " + b +" is " + Math.max(a,b));
+      System.out.println("Max of " + c +" and " + d +" is " + Math.max(c,d));
+      System.out.println("Max of " + e +" and " + f +" is " + Math.max(x,y));
+      System.out.println("Max of " + g +" and " + h +" is " + Math.max(r,s));
+   }
+}
