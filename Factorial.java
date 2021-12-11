be4e82948e7b18ad6b5ca59dde3fbe3bff8c320a
Mohammad Arslaan <95520101+Arslaanqayoom@users.noreply.github.com>
2021-12-11 18:33:36 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+Public class numbers {
+    public static void main(String[] args) 
+    {
+        int number = 8;
+        long fact = 1;
+        for(int i = 1; i <= number; i++)
+        {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of "+number+" is: "+fact);
+    }
+}
