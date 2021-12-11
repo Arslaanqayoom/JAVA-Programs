2700d5af1807cd586d587b9e4f23ae393e0fd371
Mohammad Arslaan <95520101+Arslaanqayoom@users.noreply.github.com>
2021-12-11 18:42:48 +0530

    Update Areaofcircle.java

--- Areaofcircle.java
+++ Areaofcircle.java
@@ -4,7 +4,7 @@ class Circle
    static Scanner sc = new Scanner(System.in);
    public static void main(String args[])
    {
-      System.out.print("Enter the radius: ");
+      System.out.print("Enter the Radius of circle : ");
       double radius = sc.nextDouble();
       double area = Math.PI * (radius * radius);
       System.out.println("The area of circle is: " + area);
