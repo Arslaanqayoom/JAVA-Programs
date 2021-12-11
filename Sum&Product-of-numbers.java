ff7df3552ee989fa376fa9ada5746642f8deb795
Mohammad Arslaan <95520101+Arslaanqayoom@users.noreply.github.com>
2021-12-11 18:48:28 +0530

    Create Sum&Product-of-numbers.java

---
+++ Sum&Product-of-numbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;	
+public class Digits {
+	public static void main(String args[]) 
+  {
+	  Scanner in = new Scanner(System.in); 
+	  System.out.println("Sum and Product of numbers");	  
+	  System.out.print("Enter a Number: ");
+	  int number = in.nextInt();
+	  int remainder, temp, sum=0, product=1;
+	  temp = number;
+	  while (temp!=0){
+		  remainder = temp % 10;
+		  sum = sum + remainder;
+		  product = product * remainder;
+		  temp = temp / 10;
+	  }
+	  System.out.println("Sum of digits of Number '"+number+"'': "+sum);
+	  System.out.println("Product of digits of Number '"+number+"'': "+product);
+	}
+ }
